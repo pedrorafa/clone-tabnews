500ff7f8f75f004650750240df4cfb11dfcd5cdd
Pedro Rafael <pedrorafag00@gmail.com>
2025-03-19 18:16:08 +0000

    add on `status`end-point `connection-info`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.Query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ teste: "valor" });
+  const dbVersionQuery = await database.Query("Show server_version;");
+  const dbVersion = dbVersionQuery.rows[0].server_version;
+
+  const dbMaxConnectionsQuery = await database.Query("Show max_connections");
+  const dbMaxConnections = dbMaxConnectionsQuery.rows[0].max_connections;
+
+  const dbOpenedConnetionsQuery = await database.Query(
+    "Select * from pg_stat_activity",
+  );
+  const dbOpenedConnetions = dbOpenedConnetionsQuery.rows.filter(
+    (conn) =>
+      conn.usename === process.env.POSTGRES_USER && conn.state === "active",
+  ).length;
+
+  const updatedAt = new Date().toISOString();
+
+  const body = {
+    updatedAt: updatedAt,
+    dependencies: {
+      database: {
+        version: Number(dbVersion),
+        max_connections: Number(dbMaxConnections),
+        opened_connections: Number(dbOpenedConnetions),
+      },
+    },
+  };
+
+  response.status(200).json(body);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.updatedAt).toBeDefined();
+
+  const database = body.dependencies.database;
+
+  expect(database.version).toBe(16);
+  expect(database.max_connections).toBe(100);
+  expect(database.opened_connections).toBe(1);
 });
