3b75809437387f3b5d89a544c35edef9beefaff2
Pedro Rafael <pedrorafag00@gmail.com>
2025-04-12 18:40:51 +0000

    Refactor of local test orchestration

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,7 +2,8 @@ import retry from "async-retry";
 import database from "infra/database";
 
 function getWebServerQuery(query) {
-  return `http://localhost:3000${query}`;
+  if (query[0] === "/" || query[0] === "\\") query = query.substr(1);
+  return `http://localhost:3000/${query}`;
 }
 
 async function waitForAllServices() {
@@ -11,13 +12,20 @@ async function waitForAllServices() {
   async function waitForWebServices() {
     return await retry(fetchWebServerStatus, {
       retries: 100,
-      minTimeout: 100,
       maxTimeout: 1000,
     });
 
-    async function fetchWebServerStatus() {
-      const response = await fetch(getWebServerQuery("/api/v1/status"));
-      await response.json();
+    async function fetchWebServerStatus(bail, triesCount) {
+      if (triesCount === 25)
+        console.log(
+          `We already made ${triesCount} request's on server! Be sure that server builder is running.`,
+        );
+
+      const response = await fetch(getWebServerQuery("api/v1/status"));
+      if (response.ok) return await response.json();
+      throw new Error(
+        `Error on request "/status" status code: ${response.status}`,
+      );
     }
   }
 }
