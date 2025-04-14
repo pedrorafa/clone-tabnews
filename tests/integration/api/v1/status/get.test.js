092e1c5b912239f8fe20eb6a571608501889948b
Pedro Rafael <pedrorafag00@gmail.com>
2025-04-14 12:01:00 +0000

    upload a error in  to validate workflow

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
     orchestrator.getWebServerQuery("/api/v1/status"),
   );
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(500);
 
   const body = await response.json();
 
