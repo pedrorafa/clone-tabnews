5176991f30faabf44b03cd34bfe54e1cc27ae223
Pedro Rafael <pedrorafag00@gmail.com>
2025-04-15 20:29:26 +0000

    Resolve `ESlint` warnings

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,17 +2,20 @@ import migrationRunner from "infra/migrationRunner.js";
 
 export default async function migrations(request, response) {
   switch (request.method) {
-    case "POST":
+    case "POST": {
       const migrations = await migrationRunner.executeMigrations(false);
       const status = migrations.length > 0 ? 201 : 200;
 
       response.status(status).json(migrations);
       break;
-    case "GET":
+    }
+    case "GET": {
       response.status(200).json(await migrationRunner.executeMigrations(true));
       break;
-    default:
+    }
+    default: {
       response.status(405).end();
       break;
+    }
   }
 }
